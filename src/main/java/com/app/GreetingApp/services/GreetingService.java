d2a568041b4f2c3aaecf182f920ba80bfb4834c0
virajkushwaha <virajkushwaha21@gmail.com>
2025-02-24 15:52:56 +0530

    [Viraj] Add .Adding UC1_GreetApp

--- src/main/java/com/app/GreetingApp/services/GreetingService.java
+++ src/main/java/com/app/GreetingApp/services/GreetingService.java
@@ -1,4 +1,10 @@
 package com.app.GreetingApp.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingService {
+    public String getGreeting(){
+        return "Hello World";
+    }
 }
