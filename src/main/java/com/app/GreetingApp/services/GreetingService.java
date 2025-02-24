21baa8c64c632c854c50439fd950327a68b19f90
virajkushwaha <virajkushwaha21@gmail.com>
2025-02-24 16:34:36 +0530

    [Viraj] Add .Adding UC2_GreetApp

--- src/main/java/com/app/GreetingApp/services/GreetingService.java
+++ src/main/java/com/app/GreetingApp/services/GreetingService.java
@@ -1,10 +1,16 @@
 package com.app.GreetingApp.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
-    public String getGreeting(){
+
+    private static final Logger logger = LoggerFactory.getLogger(GreetingService.class);
+
+    public String getGreeting() {
+        logger.info("Generating greeting message from GreetingService");
         return "Hello World";
     }
 }
